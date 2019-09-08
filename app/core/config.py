e38d1cf756cca2b845d15197ba0137df4a40c1d2
Rizha Musthafa <rizha.musthafa@gmail.com>
2019-09-08 22:04:56 +0700

    add config

---
+++ app/core/config.py
@@ -0,0 +1,24 @@
+import os
+
+
+API_V1_STR = "/api/v1"
+
+SECRET_KEY = os.getenvb(b"SECRET_KEY")
+if not SECRET_KEY:
+    SECRET_KEY = os.urandom(32)
+
+ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24 * 8  # 60 minutes * 24 hours * 8 days = 8 days
+
+BACKEND_CORS_ORIGINS = os.getenv(
+    "BACKEND_CORS_ORIGINS"
+)  # a string of origins separated by commas, e.g: "http://localhost, http://localhost:4200, http://localhost:3000, http://localhost:8080, http://local.dockertoolbox.tiangolo.com"
+
+SENTRY_DSN = os.getenv("SENTRY_DSN")
+
+POSTGRES_SERVER = os.getenv("POSTGRES_SERVER")
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+POSTGRES_DB = os.getenv("POSTGRES_DB")
+SQLALCHEMY_DATABASE_URI = (
+    f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}/{POSTGRES_DB}"
+)
